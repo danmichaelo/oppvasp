eb6f2feedc89a0bd703b4901124f3463d149cd53
danmichaelo <danmichaelo@gmail.com>
2011-05-16 13:51:35 +0200

    Added version check

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
 """ Oslo Python Package for VASP """
 
+from sys import version_info
+ver = version_info[0]*10 + version_info[1]
+if ver < 26:
+    raise Exception("oppvasp requires python version 2.6 or higher")
+
+
 import numpy as np
 import time
 
